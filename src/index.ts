d749cb95b0318b77bcdea7366fc15e0d275370f2
lilyanb <lilyan.bastien@gmail.com>
2024-07-24 13:17:28 +0200

    style: run format

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import RedstoneAdapter from "./adapters/oracles/redstone-adapter/RedstoneAdapter.js";
+import RedstoneAdapter from './adapters/oracles/redstone-adapter/RedstoneAdapter.js';
 
 async function getPriceAndLog(redstoneAdapter: RedstoneAdapter) {
-  try {
-    const data = await redstoneAdapter.getLatestPrice();
-    console.log(data);
-  } catch (error) {
-    console.error("Error fetching the latest price:", error);
-  }
+    try {
+        const data = await redstoneAdapter.getLatestPrice();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching the latest price:', error);
+    }
 }
 
 async function main() {
-  const redstoneAdapter = new RedstoneAdapter();
-  await getPriceAndLog(redstoneAdapter);
-  setInterval(() => {
-    getPriceAndLog(redstoneAdapter);
-  }, 5000);
+    const redstoneAdapter = new RedstoneAdapter();
+    await getPriceAndLog(redstoneAdapter);
+    setInterval(() => {
+        getPriceAndLog(redstoneAdapter);
+    }, 5000);
 }
 
 main();
