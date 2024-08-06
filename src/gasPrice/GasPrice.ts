1feef31647cb627f545c128e3553e6494451f6d8
lilyanb <lilyan.bastien@gmail.com>
2024-08-06 13:37:40 +0200

    style: refacto code

--- src/gasPrice/GasPrice.ts
+++ src/gasPrice/GasPrice.ts
@@ -1,17 +1,36 @@
+import type { PublicClient } from "viem";
+import { newClient } from "../utils/index.ts";
 import type IGasPrice from "./IGasPrice.js";
 import { GasPriceFetchingError } from "./types.js";
-import { createPublicClient, http } from "viem";
-import { mainnet } from "viem/chains";
 
 export default class GasPrice implements IGasPrice {
-  publicClient = createPublicClient({
-    chain: mainnet,
-    transport: http(),
-  });
+  private client: PublicClient | null = null;
+
+  constructor() {
+    const rpcUrl = process.env.RPC_URL;
+    if (!rpcUrl) {
+      throw new Error("RPC URL not set");
+    }
+
+    this.initializeClient(rpcUrl);
+  }
+
+  private async initializeClient(rpcUrl: string) {
+    try {
+      this.client = await newClient(rpcUrl);
+    } catch (error) {
+      console.error("Failed to initialize public client:", error);
+      throw new Error("Failed to initialize public client");
+    }
+  }
 
   /** @inheritDoc */
   async getGasPrice() {
-    const gasPrice = await this.publicClient.getGasPrice();
+    if (!this.client) {
+      throw new Error("Public client not initialized");
+    }
+
+    const gasPrice = await this.client.getGasPrice();
     if (!gasPrice) {
       throw new GasPriceFetchingError();
     }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,17 @@
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
+
 export async function sleep(milliseconds: number) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, milliseconds);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+export async function newClient(rpcUrl: string) {
+  const publicClient = createPublicClient({
+    chain: mainnet,
+    transport: http(rpcUrl),
+  });
+
+  return publicClient;
 }
