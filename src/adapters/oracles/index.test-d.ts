fa3da8feaf00f8d916f53457734676c9c4078851
kirienzo-eth <kirienzo.eth@gmail.com>
2024-10-07 13:41:27 +0200

    refactor(oracle-adapters): let type inference for the exports and change the test to a toMatchTypeOf

--- src/adapters/oracles/index.test-d.ts
+++ src/adapters/oracles/index.test-d.ts
@@ -5,7 +5,7 @@ describe('adapters/oracles/index', () => {
     it('should export instances of IOracleAdapter', async () => {
         const adapters = await import('./index.js');
         for (const oracle of Object.values(adapters)) {
-            expectTypeOf(oracle).toEqualTypeOf<IOracleAdapter>();
+            expectTypeOf(oracle).toMatchTypeOf<IOracleAdapter>();
         }
     });
 });
