1510b3af554f1740221be3545c2b8d7a220f84a9
kirienzo-eth <kirienzo.eth@gmail.com>
2024-10-10 08:56:21 +0200

    test(oracle-adapters): make the typing test more precise

--- src/adapters/oracles/index.test-d.ts
+++ src/adapters/oracles/index.test-d.ts
@@ -1,11 +1,11 @@
 import { describe, expectTypeOf, it } from 'vitest';
-import type AOracleAdapter from './AOracleAdapter.ts';
+import type { AHighLatencyOracle, ALowLatencyOracle } from './AOracleAdapter.ts';
 
 describe('adapters/oracles/index', () => {
     it('should export instances of OracleAdapter', async () => {
         const adapters = await import('./index.js');
         for (const oracle of Object.values(adapters)) {
-            expectTypeOf(oracle).toMatchTypeOf<AOracleAdapter>();
+            expectTypeOf(oracle).toMatchTypeOf<ALowLatencyOracle | AHighLatencyOracle>();
         }
     });
 });
